5d6a38aec4498c7afd7be8076604fb5ac1d323d8
Gabriel <silva.gabrielmelosantos@gmail.com>
2023-07-31 11:47:05 -0300

    criando subscriber e publisher para a fila de emissao de cartoes

---
+++ msavaliadorcredito/src/main/java/com/gabriel/msavaliadorcredito/infra/mqueue/SolicitacaoEmissaoCartaoPublisher.java
@@ -0,0 +1,31 @@
+package com.gabriel.msavaliadorcredito.infra.mqueue;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gabriel.msavaliadorcredito.model.DadosSolicitacaoEmissaoCartao;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SolicitacaoEmissaoCartaoPublisher {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final Queue queueEmissaoCartoes;
+
+	public void SolicitarCartao(DadosSolicitacaoEmissaoCartao dados) throws JsonProcessingException {
+		var json = convertIntoJson(dados);
+		rabbitTemplate.convertAndSend(queueEmissaoCartoes.getName(), json);
+	}
+
+	private String convertIntoJson(DadosSolicitacaoEmissaoCartao dados) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		var json = mapper.writeValueAsString(dados);
+		return json;
+	}
+
+}
---
+++ mscartoes/src/main/java/com/gabriel/mscartoes/infra/mqueue/EmissaoCartoesSubscriber.java
@@ -0,0 +1,15 @@
+package com.gabriel.mscartoes.infra.mqueue;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmissaoCartoesSubscriber {
+
+	@RabbitListener(queues = "${mq.queues.emissao-cartoes}")
+	public void ReceberSolicitacaoEmissao(@Payload String payload) {
+		System.out.println(payload);
+	}
+	
+}
